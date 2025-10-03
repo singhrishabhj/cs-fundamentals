bde57e862da00b563b744da68e05e8396db5ca1f
RISHABH SINGH <152042945+singhrishabhj@users.noreply.github.com>
2025-10-03 14:30:48 +0530

    Create 02_VariableDeclaration.java

---
+++ problem_solving/java/50question/02_VariableDeclaration.java
@@ -0,0 +1,22 @@
+public class  VVariableDeclaration{
+ public static void main (String [] args)
+  {
+    //Initialsing all the variables
+    byte b = 8;
+    int i = 0;
+    float f = 3.14f;
+    long l = 9000L;
+    double d = 90.09876;
+    boolean bool = false;
+    char c = 'A';
+
+        // Printing all the variables individually 
+        System.out.println("byte b = " + b);
+        System.out.println("int i = " + i);
+        System.out.println("float f = " + f);
+        System.out.println("long l = " + l);
+        System.out.println("double d = " + d);
+        System.out.println("char c = " + c);
+        System.out.println("boolean bool = " + bool);
+  }
+}
